086785ce00a029b491def71f2b9defeded9677b2
lucaspbds <118389176+lucaspbds@users.noreply.github.com>
2024-02-10 14:02:52 -0300

    Function playPauseDecider created

--- script.js
+++ script.js
@@ -1,13 +1,52 @@
 const songName = document.getElementById("song-name");
 const bandName = document.getElementById("band-name");
+const cover = document.getElementById("cover");
 const song = document.getElementById("audio");
-const play = document.getElementById("play")
+const play = document.getElementById("play");
+let playlist = []
 
-songName.innerText = "Sobre-Exaltado";
-bandName.innerText = "Comunidade Católica Shalom";
+const vienna = {
+    songName: "Vienna",
+    artist: "Billy Joel", 
+    file: "Billy Joel - Vienna.mp3"
+};
+
+const youCouldBeMine = {
+    songName: "You Could Be Mine",
+    artist: "Guns N Roses",
+    file: "Guns N Roses  - You Could Be Mine"
+};
+
+const sobreExaltado = {
+    songName: "Sobre-Exaltado",
+    artist: "Comunidade Católica Shalom",
+    file: "Shalom - Sobre-Exaltdo"
+};
+
+let isPlaying = false;
+
 
 let playMusic = () => {
+    play.querySelector(".bi").classList.remove("bi-play-circle-fill");
+    play.querySelector(".bi").classList.add("bi-pause-circle-fill");
     song.play();
+    isPlaying = true;
 }
 
-play.addEventListener("click", playMusic);
+let pauseMusic = () => {
+    play.querySelector(".bi").classList.remove("bi-pause-circle-fill");
+    play.querySelector(".bi").classList.add("bi-play-circle-fill");
+    song.pause();
+    isPlaying = false;
+}
+
+let playPauseDecider = () => {
+    if (isPlaying === true) {
+        pauseMusic();
+    }
+    else{
+        playMusic();
+    }
+}
+
+play.addEventListener("click", playPauseDecider);
